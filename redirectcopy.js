0b1956b8ca77b4592ee61315a2b9270ff274ae1d
tientran863913 <66064403+tientran863913@users.noreply.github.com>
2020-05-29 14:48:23 +0700

    Add files via upload

--- redirectcopy.js
+++ redirectcopy.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='https://trankimtienvn.blogspot.com/'>https://trankimtienvn.blogspot.com/"+document.location.pathname+"</a>";
+    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='https://www.facebook.com/trankimtien.vn.zaiiidepbanbds/"</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
