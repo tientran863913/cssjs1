72367cc1b2f31c1928f213b16a5589d71d1975df
tientran863913 <66064403+tientran863913@users.noreply.github.com>
2020-05-29 15:51:29 +0700

    Add files via upload

--- redirectcopy.js
+++ redirectcopy.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='https://www.facebook.com/trankimtien.vn.zaiiidepbanbds/"</a>";
+    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='http://trankimtien.vn/"</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
@@ -15,3 +15,9 @@ function FSAddOriginalLink(){
     },0);
 }
 document.oncopy = FSAddOriginalLink;
+
+<img alt="" src="/gallery/images/TIEN/Tân Bửu/Tân Bửu 4/dat-nen-tan-buu-0904863913-13.jpg" style="height:236px; width:640px">
+
+http://trankimtien.vn/gallery/images/TIEN/T%C3%A2n%20B%E1%BB%ADu/T%C3%A2n%20B%E1%BB%ADu%204/dat-nen-tan-buu-0904863913-13.jpg
+
+https://faceseo.vn'><img align='right' height='300px' src='https://centralland.com.vn/wp-content/uploads/2020/05/dat-nen-phu-an-thanh-water-point-zaiii-dep-ban-bds-0904863913-18.jpg
